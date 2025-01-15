ee521a99ff39f4b5c68d36b09ca76c76907dd3f3
VINERAJ <47723941+VINERAJ@users.noreply.github.com>
2025-01-14 21:14:16 -0800

    redirect working

--- country-guessing/src/App.js
+++ country-guessing/src/App.js
@@ -1,29 +1,52 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
+import {
+  BrowserRouter,
+  useNavigate,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 import Papa from 'papaparse';
 import './App.css';
+import Game from './Game.js';
 
 var countries;
 
-function MyButton() {
-  function clicked() {
-    fetch(
-      'https://raw.githubusercontent.com/VINERAJ/country-guessing/refs/heads/main/All%20Countries.csv'
-    )
-      .then(response => response.text())
-      .then(data => {
-        countries = Papa.parse(data).data;
-        console.log(countries[1][0]);
-      });
+// function MyButton() {
+//   const navigate = useNavigate();
+//   function Clicked() {
+//     navigate.navigate("Game");
+//     // fetch(
+//     //   'https://raw.githubusercontent.com/VINERAJ/country-guessing/refs/heads/main/All%20Countries.csv'
+//     // )
+//     //   .then(response => response.text())
+//     //   .then(data => {
+//     //     countries = Papa.parse(data).data;
+//     //     console.log(countries[1][0]);
+//     //   });
+//   }
+//   return (
+//     <button onClick={Clicked} className="my-button">
+//       Click to begin!
+//     </button>
+//   );
+// }
+
+function HandleClick() {
+  const navigate = useNavigate();
+  function Clicked() {
+    navigate('/game');
   }
   return (
-    <button onClick={clicked} className="my-button">
+    <button onClick={Clicked} className="my-button">
       Click to begin!
     </button>
   );
 }
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <header className="App-header">
@@ -39,7 +62,10 @@ function App() {
         >
           Learn React
         </a>
-        <MyButton />
+        <HandleClick />
+        <Routes>
+          <Route path="/game" element={<Game />}></Route>
+        </Routes>
       </header>
     </div>
   );
